994398c3e16264c9528404872071c736d06fce7c
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-16 14:16:17 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,13 +1,10 @@
 def checkRange(value,low,high):
     if value < low or value > high:
-        print("Attribute ",value ," is not in range")
         return False
     return True
 
 def battery_is_ok(temperature,soc,charge_rate):
     result=checkRange(temperature,0,45) and checkRange(soc,20,80) and checkRange(charge_rate,0,0.8)
-    if result ==True:
-        print("Battery is in normal condition")
     return result
 
 if __name__ == '__main__':
