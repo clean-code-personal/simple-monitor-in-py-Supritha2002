e30dfb45ef7eccdd897667d65937b63a2e208156
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-16 14:26:56 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,9 +1,9 @@
-def checkRange(value,low,high):
+def checkRange(input,value,low,high):
     if value < low or value > high:
         return False
     return True
 
 def battery_is_ok(temperature,soc,charge_rate):
-    result=checkRange(temperature,0,45) and checkRange(soc,20,80) and checkRange(charge_rate,0,0.8)
+    result=checkRange('Temperature',temperature,0,45) and checkRange('SOC',soc,20,80) and checkRange('Charge_rate',charge_rate,0,0.8)
     return result
 
