2cef6003332a5591498e49c9c88c06fda68377bb
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-16 14:36:38 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -14,5 +14,7 @@ def status(attribute,limit):
     print(attribute," is ",limit)
 def battery_is_ok(temperature,soc,charge_rate):
     result=checkRange('Temperature',temperature,0,45) and checkRange('SOC',soc,20,80) and checkRange('Charge_rate',charge_rate,0,0.8)
+       if result ==True:
+        print("Battery is in normal condition")
     return result
 
