0d76e2dda51ccca6169b0ebebcec9146892eff89
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-16 13:23:34 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -8,10 +8,7 @@ def checkRange(input,value,low,high):
     return True
 
 def battery_is_ok(temperature,soc,charge_rate):
-    result1=checkRange('Temperature',temperature,0,45)
-    result2=checkRange('SOC',soc,20,80)
-    result3=checkRange('Charge_Rate',charge_rate,0,0.8)
-    result=result1 and result2 and result3
+    result=checkRange('Temperature',temperature,0,45) and checkRange('SOC',soc,20,80) and checkRange('Charge_Rate',charge_rate,0,0.8)
     if result ==True:
         print("Battery is in normal condition")
     return result
