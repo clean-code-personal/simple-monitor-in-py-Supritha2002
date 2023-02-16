49b1075f7c27aa80300ecca23785dc4b075b7f4d
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-16 14:46:50 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,13 @@
+from battery_report_status import *
 def checkRange(input,value,low,high):
     if value < low:
-        status(input,'low')
+        attribute_status(input,'low')
         return False
     if value > high:
-        status(input,'high')
+        attribute_status(input,'high')
         return False
     return True
-def status(attribute,limit):
-    print(attribute," is ",limit)
-def battery_normal(result):
-    if result==True:
-        print("s")
 def battery_is_ok(temperature,soc,charge_rate):
     result=checkRange('Temperature',temperature,0,45) and checkRange('SOC',soc,20,80) and checkRange('Charge_rate',charge_rate,0,0.8)
     battery_normal(result)
     return result
-
