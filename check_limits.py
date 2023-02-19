5d96e4d645eedbf2cec971a80a6f6d213b82d744
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-19 15:18:53 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -6,17 +6,18 @@ def checkRange(input,value,low,high):
         attribute_status(input,'high')
         return False
     return True
-def attribute_status(attribute,limit):
+def attribute_status(attribute,limit):    #extension2:supports multiple languages to print user messages for abnormal condition
     german_lang={'Temperature':'Temperatur','SOC':'Ladezustand','Charge_rate':'Ladestrom','low':'niedrig','high':'hoch'}
     hindi_lang={'Temperature': 'तापमान','SOC':'चार्ज स्थिति','Charge_rate': 'चार्ज दर'}
     print(german_lang[attribute]," is ",german_lang[limit])
     
-def battery_normal(result):
+def battery_normal(result):     #extension2:supports multiple languages to print user messages for normal condition
     german_status="Batterie ist in normalem Zustand"
     english_status="Battery in normal condition"
     if result==True:
         print(german_status)
-def convertoCelcius(temperature,unit):
+        
+def convertoCelcius(temperature,unit):  #extension3 - accepts temperature in different unit
     return  temperature if unit == 'Celcius' else  (temperature - 32) * (5/9)
 
 def battery_is_ok(temperature,tempunit,soc,charge_rate):
