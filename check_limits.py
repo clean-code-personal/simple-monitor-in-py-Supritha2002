788ad32dbb0d621a4c23c2f4c7a585913e18815d
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-19 22:06:48 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,27 +1,49 @@
-def checkRange(input,value,low,high):
+def checkRange(input,value,low,high,lang):
     if value < low:
-        attribute_status(input,'low')
+        attribute_status(input,'low',lang)
         return False
     if value > high:
-        attribute_status(input,'high')
+        attribute_status(input,'high',lang)
         return False
     return True
-def attribute_status(attribute,limit):    #extension2:supports multiple languages to print user messages for abnormal condition
-    german_lang={'Temperature':'Temperatur','SOC':'Ladezustand','Charge_rate':'Ladestrom','low':'niedrig','high':'hoch'}
-    hindi_lang={'Temperature': 'तापमान','SOC':'चार्ज स्थिति','Charge_rate': 'चार्ज दर'}
-    print(german_lang[attribute]," is ",german_lang[limit])
+
+def attribute_status(attribute,limit,lang):
+    language={
+    'german':{'Temperature':'Temperatur',
+              'SOC':'Ladezustand',
+              'Charge_rate':'Ladestrom',
+              'low':'niedrig',
+              'high':'hoch'},
+    'hindi':{'Temperature': 'तापमान',
+             'SOC':'चार्ज स्थिति',
+             'Charge_rate': 'चार्ज दर',
+             'low':'कम',
+             'high':'उच्च'
+             },
+    'english':{'Temperature':'Temperature',
+               'SOC' : 'State Of Charge'  ,
+               'Charge_rate': 'Charge Rate',
+               'low':'low',
+               'high':'high'
+              }
+
+    }
+    print(language[lang][attribute]," is ",language[lang][limit])
     
-def battery_normal(result):     #extension2:supports multiple languages to print user messages for normal condition
-    german_status="Batterie ist in normalem Zustand"
-    english_status="Battery in normal condition"
+def battery_normal(result,lang):
+    status_language= {'german' :'Batterie ist in normalem Zustand',
+                     'hindi'  :'सामान्य स्थिति में बैटरी'              ,
+                     'english':'Battery in normal condition'    
+                     }
     if result==True:
-        print(german_status)
+        print(status_language[lang])
+
         
 def convertoCelcius(temperature,unit):  #extension3 - accepts temperature in different unit
     return  temperature if unit == 'Celcius' else  (temperature - 32) * (5/9)
 
-def battery_is_ok(temperature,tempunit,soc,charge_rate):
+def battery_is_ok(temperature,tempunit,soc,charge_rate,lang):
     temperature=convertoCelcius(temperature,tempunit)
-    result=checkRange('Temperature',temperature,0,45) and checkRange('SOC',soc,20,80) and checkRange('Charge_rate',charge_rate,0,0.8)
-    battery_normal(result)
+    result=checkRange('Temperature',temperature,0,45,lang) and checkRange('SOC',soc,20,80,lang) and checkRange('Charge_rate',charge_rate,0,0.8,lang)
+    battery_normal(result,lang)
     return result
