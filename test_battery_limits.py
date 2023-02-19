b24ee58f54031f84154dc320f55ce7ab54072528
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-19 15:12:33 +0530

    Update test_battery_limits.py

--- test_battery_limits.py
+++ test_battery_limits.py
@@ -2,12 +2,10 @@ import unittest
 from check_limits import *
 
 class TestBattery(unittest.TestCase):
-    def test_battery_status(self):
-        battery_is_ok(25,70,0.7)
-        battery_is_ok(50, 75, 0)
+    
     def test_battery_condition(self):
-        self.assertTrue(battery_is_ok(25, 70, 0.7))
-        self.assertFalse(battery_is_ok(50, 75, 0))
+        self.assertTrue(battery_is_ok(25,'Celcius', 70, 0.7))
+        self.assertFalse(battery_is_ok(120,'fahrenheit', 75, 0))
 
 if __name__ == '__main__':
     unittest.main()
